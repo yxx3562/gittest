01af2d19fc7820d42605316d06e28107c6950c27
yanxun <yanxun@gmail.com>
2020-08-20 23:23:42 +0800

    第一次提交测试的git项目

---
+++ src/test/java/com/yanxun/gittest/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanxun.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/yanxun/gittest/test/Test.java
@@ -0,0 +1,13 @@
+package com.yanxun.gittest.test;
+
+/**
+ * @author YX3562
+ * @create 2020-08-20 22:41
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String str="大山工作室测试Git!";
+        System.out.println(str);
+    }
+}
